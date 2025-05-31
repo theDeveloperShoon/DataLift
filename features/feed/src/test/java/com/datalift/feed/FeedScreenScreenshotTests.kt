46bd8300abd3ebba2b67560dcc8faaa6d27ee370
Sean Cotter <Seancotter04@gmail.com>
2025-05-30 21:03:52 -0400

    Started work on screenshot testing for Feed

---
+++ features/feed/src/test/java/com/datalift/feed/FeedScreenScreenshotTests.kt
@@ -0,0 +1,19 @@
+package com.datalift.feed
+
+import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+import org.robolectric.annotation.LooperMode
+
+
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(application = HiltTestApplication::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+class FeedScreenScreenshotTests {
+//    @get:Rule
+//    val composeTestRule = createAndroidComposeRule<>
+}
