24168835a23e10a0c9f6a69b996b9f79b157dbb1
Sean Cotter <Seancotter04@gmail.com>
2025-05-30 20:59:43 -0400

    Modified UseRepository to return UserData

--- core/data/src/main/java/com/datalift/data/repository/CompositeUserRepository.kt
+++ core/data/src/main/java/com/datalift/data/repository/CompositeUserRepository.kt
@@ -1,11 +1,23 @@
 package com.datalift.data.repository
 
 import com.datalift.database.service.AccountService
+import com.datalift.model.data.UserData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class CompositeUserRepository @Inject constructor(
     private val accountService: AccountService
 ) : UserRepository {
+    override val userData: Flow<UserData>
+        get() = accountService.user.map { user ->
+            UserData(
+                userID = user.id,
+                userName = user.name ?: "",
+                userProfileUrl = user.profileUrl
+            )
+        }
+
     override fun getCurrentUserId(): String =
         accountService.currentUserId
 }
--- core/data/src/main/java/com/datalift/data/repository/UserRepository.kt
+++ core/data/src/main/java/com/datalift/data/repository/UserRepository.kt
@@ -1,6 +1,10 @@
 package com.datalift.data.repository
 
+import com.datalift.model.data.UserData
+import kotlinx.coroutines.flow.Flow
+
 interface UserRepository {
+    val userData: Flow<UserData>
     fun getCurrentUserId(): String
 //    fun getUser(id: String) : Flow<>
 }
