c508854016ea754385133d6e1873d8c7bb82fb91
Sean Cotter <Seancotter04@gmail.com>
2025-06-03 00:01:17 -0400

    Added workouts for usage inside a ParamterProvider

--- core/ui/src/main/java/com/datalift/ui/PostPreviewParameterProvider.kt
+++ core/ui/src/main/java/com/datalift/ui/PostPreviewParameterProvider.kt
@@ -1,9 +1,14 @@
 package com.datalift.ui
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.datalift.model.data.Exercise
+import com.datalift.model.data.ExerciseSet
+import com.datalift.model.data.MuscleGroup
 import com.datalift.model.data.Post
 import com.datalift.model.data.PostResource
 import com.datalift.model.data.UserData
+import com.datalift.model.data.Workout
+import com.datalift.model.data.WorkoutResource
 import com.datalift.ui.PreviewParameterData.posts
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
@@ -63,5 +68,146 @@ object PreviewParameterData {
             ),
             userData = userData
         ),
+    )
+
+    val workouts = listOf(
+        Workout(
+           workoutResource =  WorkoutResource(
+               workoutId = "1",
+               workoutName = "Chest and Push",
+               date = Instant.parse ("2025-05-14T00:00:00.000Z"),
+               muscleGroups = listOf(MuscleGroup.Chest.displayName, MuscleGroup.Push.displayName),
+               exercises = listOf(
+                   Exercise(
+                       name = "Bench Press",
+                       bodyPart = "Chest",
+                       sets = listOf(
+                           ExerciseSet(
+                               reps = 10,
+                               weight = 200.0
+                           ),
+                           ExerciseSet(
+                               reps = 10,
+                               weight = 200.0
+                           ),
+                           ExerciseSet(
+                               reps = 10,
+                               weight = 200.0
+                           )
+                       )
+                   ),
+                   Exercise(
+                       name = "Incline Bench Press",
+                       bodyPart = "Chest",
+                       sets = listOf(
+                           ExerciseSet(
+                               reps = 10,
+                               weight = 200.0
+                           ),
+                           ExerciseSet(
+                               reps = 10,
+                               weight = 200.0
+                           ),
+                           ExerciseSet(
+                               reps = 10,
+                               weight = 200.0
+                           ),
+                       )
+                   )
+               )
+           )
+        ),
+        Workout(
+            workoutResource = WorkoutResource(
+                workoutId = "2",
+                workoutName = "Pull and Legs",
+                date = Instant.parse("2025-05-13T00:00:00.000Z"),
+                muscleGroups = listOf(MuscleGroup.Pull.displayName, MuscleGroup.Legs.displayName),
+                exercises = listOf(
+                    Exercise(
+                        name = "Pull Ups",
+                        bodyPart = "Pull",
+                        sets = listOf(
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 0.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 0.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 0.0
+                            )
+                        )
+                    ),
+                    Exercise(
+                        name = "Leg Press",
+                        bodyPart = "Legs",
+                        sets = listOf(
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 100.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 100.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 100.0
+                            )
+                        )
+                    ),
+                    Exercise(
+                        name = "Leg Extensions",
+                        bodyPart = "Legs",
+                        sets = listOf(
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 100.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 100.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 100.0
+                            )
+                        )
+                    )
+                )
+            )
+        ),
+        Workout(
+            workoutResource = WorkoutResource(
+                workoutId = "3",
+                workoutName = "Cardio",
+                date = Instant.parse("2025-05-12T00:00:00.000Z"),
+                muscleGroups = listOf(MuscleGroup.Cardio.displayName),
+                exercises = listOf(
+                    Exercise(
+                        name = "Running",
+                        bodyPart = "Cardio",
+                        sets = listOf(
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 0.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 0.0
+                            ),
+                            ExerciseSet(
+                                reps = 10,
+                                weight = 0.0
+                            )
+                        )
+                    )
+                )
+            )
+        )
     )
 }
