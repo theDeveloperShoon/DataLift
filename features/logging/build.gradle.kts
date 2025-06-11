3482c1151eaab33df404829b493a15bc44e577bb
Sean Cotter <Seancotter04@gmail.com>
2025-06-11 14:53:44 -0400

    Removed unused implementations/duplicated implementations

--- features/logging/build.gradle.kts
+++ features/logging/build.gradle.kts
@@ -8,10 +8,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(projects.core.data)
+
 }
