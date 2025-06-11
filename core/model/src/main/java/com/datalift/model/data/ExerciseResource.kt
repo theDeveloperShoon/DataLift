f20e7aeeaea58db370d49ca9ae8c0ca74080459b
Sean Cotter <Seancotter04@gmail.com>
2025-06-11 14:40:33 -0400

    Added ExerciseResource to resemble the existing ExerciseItem already existent in the database

---
+++ core/model/src/main/java/com/datalift/model/data/ExerciseResource.kt
@@ -0,0 +1,10 @@
+package com.datalift.model.data
+
+data class ExerciseResource(
+    val title: String = "",
+    val type: String = "",
+    val description: String = "",
+    val bodyPart: String = "",
+    val equipment: String = "",
+    val level: String = "",
+)
