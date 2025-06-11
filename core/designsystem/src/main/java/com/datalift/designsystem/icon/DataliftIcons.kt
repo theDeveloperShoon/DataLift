74eb5f023baae6879e756d167383dcb8c76970d1
Sean Cotter <Seancotter04@gmail.com>
2025-06-11 14:45:04 -0400

    Added Icons for Trashing, Increasing and Decreasing, Collapsing and Expanding

--- core/designsystem/src/main/java/com/datalift/designsystem/icon/DataliftIcons.kt
+++ core/designsystem/src/main/java/com/datalift/designsystem/icon/DataliftIcons.kt
@@ -2,22 +2,26 @@ package com.datalift.designsystem.icon
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.EmojiEvents
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PersonAdd
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.ChevronRight
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.EmojiEvents
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.Search
 
 
@@ -26,19 +30,24 @@ object DataliftIcons {
     val Calendar = Icons.Default.CalendarMonth
     val Checked = Icons.Filled.Check
     val Close = Icons.Default.Close
+    val Collapsed = Icons.AutoMirrored.Outlined.KeyboardArrowLeft
+    val Decrease = Icons.Default.Remove
     val Edit = Icons.Default.Edit
     val EditBorder = Icons.Outlined.Edit
+    val Expanded = Icons.Outlined.KeyboardArrowDown
     val ExpandMore = Icons.Outlined.ChevronRight
     val Home = Icons.Default.Home
     val HomeBorder = Icons.Outlined.Home
     val Heart = Icons.Filled.Favorite
     val HeartBorder = Icons.Outlined.FavoriteBorder
+    val Increase = Icons.Default.Add
     val Magnifier = Icons.Default.Search
     val MagnifierBorder = Icons.Outlined.Search
     val NavigateUp = Icons.AutoMirrored.Filled.ArrowBack
     val PersonAdd = Icons.Filled.PersonAdd
     val PersonCheck = Icons.Filled.CheckCircle
     val Search = Icons.Filled.Search
+    val Trash = Icons.Default.Delete
     val Trophy = Icons.Filled.EmojiEvents
     val TrophyOutlined = Icons.Outlined.EmojiEvents
 }
