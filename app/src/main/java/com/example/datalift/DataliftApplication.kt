5ba4c698a01ddb12c1b2f17c3427f4482795ebd7
Sean Cotter <Seancotter04@gmail.com>
2025-06-16 19:02:27 -0400

    Removed a compiler warning/error

--- app/src/main/java/com/example/datalift/DataliftApplication.kt
+++ app/src/main/java/com/example/datalift/DataliftApplication.kt
@@ -4,4 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DataliftApplication : Application()
+class DataliftApplication : Application() {
+
+}
