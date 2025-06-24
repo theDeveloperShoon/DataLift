91d4298dbab411aa2d08bee9e795a7be77b6843c
Sean Cotter <Seancotter04@gmail.com>
2025-06-23 23:36:27 -0400

    Modified RecentSearchQueriesUiState.Success to use RecentExerciseSearchQuery rather than string

--- features/logging/src/main/java/com/datalift/logging/RecentSearchQueriesUiState.kt
+++ features/logging/src/main/java/com/datalift/logging/RecentSearchQueriesUiState.kt
@@ -1,9 +1,11 @@
 package com.datalift.logging
 
+import com.datalift.data.model.RecentExerciseSearchQuery
+
 sealed interface RecentSearchQueriesUiState {
     data object Loading : RecentSearchQueriesUiState
 
     data class Success(
-        val recentSearchQueries: List<String> = emptyList(),
+        val recentSearchQueries: List<RecentExerciseSearchQuery> = emptyList(),
     ) : RecentSearchQueriesUiState
 }
