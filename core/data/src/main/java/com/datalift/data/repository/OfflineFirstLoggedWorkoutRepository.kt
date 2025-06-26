c4da30abc2332e61ec44b1c0cf4bb05cce6bf97d
Sean Cotter <Seancotter04@gmail.com>
2025-06-25 23:34:03 -0400

    Implemented saveWorkout in WorkoutRepository

--- core/data/src/main/java/com/datalift/data/repository/OfflineFirstLoggedWorkoutRepository.kt
+++ core/data/src/main/java/com/datalift/data/repository/OfflineFirstLoggedWorkoutRepository.kt
@@ -3,6 +3,7 @@ package com.datalift.data.repository
 import com.datalift.database.service.WorkoutService
 import com.datalift.model.data.ExerciseResource
 import com.datalift.model.data.Workout
+import com.datalift.model.data.WorkoutDraft
 import com.datalift.model.data.mapToWorkout
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -23,6 +24,10 @@ class OfflineFirstLoggedWorkoutRepository @Inject constructor(
 
     override fun queryExercise(string: String): Flow<List<ExerciseResource>> =
         workoutService.queryExercise(query = string)
+
+    override suspend fun saveWorkout(workout: WorkoutDraft) {
+        workoutService.saveWorkout(workout = workout)
+    }
 
     override suspend fun deleteWorkout(workoutId: String) {
         workoutService.deleteWorkout(workoutId = workoutId)
--- core/data/src/main/java/com/datalift/data/repository/WorkoutRepository.kt
+++ core/data/src/main/java/com/datalift/data/repository/WorkoutRepository.kt
@@ -2,11 +2,13 @@ package com.datalift.data.repository
 
 import com.datalift.model.data.ExerciseResource
 import com.datalift.model.data.Workout
+import com.datalift.model.data.WorkoutDraft
 import kotlinx.coroutines.flow.Flow
 
 interface WorkoutRepository {
     fun getLoggedWorkouts() : Flow<List<Workout>>
     fun getWorkout(workoutId: String) : Flow<Workout>
     fun queryExercise(string: String) : Flow<List<ExerciseResource>>
+    suspend fun saveWorkout(workout: WorkoutDraft)
     suspend fun deleteWorkout(workoutId: String)
 }
