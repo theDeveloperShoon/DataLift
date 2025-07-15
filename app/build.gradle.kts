1f8c12da47d8fba715775ac16939f10f9d3e866a
Sean Cotter <Seancotter04@gmail.com>
2025-07-15 12:03:45 -0400

    Updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,9 @@ plugins {
 
 android {
     namespace = "com.example.datalift"
-    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.datalift"
-        minSdk = 34
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"  // X.Y.Z, X = Major, Y = minor, Z = Patch level
 
@@ -32,13 +29,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -63,15 +53,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.testManifest)
